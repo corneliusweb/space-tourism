7df0ce2fe6083fa34f801f239fbdab0a8c02f1b3
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-08-27 14:45:58 +0100

    Change data file to .ts for type safety & Implement dynamic routing

--- src/routes/destination/$name.tsx
+++ src/routes/destination/$name.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/destination/titan')({
+export const Route = createFileRoute('/destination/$name')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/destination/titan"!</div>
+  return <div>Hello "/destination/$name"!</div>
 }
--- src/routes/destination/route.tsx
+++ src/routes/destination/route.tsx
@@ -1,9 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
+import { destinations } from '../../constants/data';
 
 export const Route = createFileRoute('/destination')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/destination"!</div>
+	return (
+		<div>
+			<p>
+				<span>01</span> <span>Pick your destination</span>
+			</p>
+
+			<img src='' alt='selected planet' />
+
+			<main>
+				<nav>
+					{destinations.map((planet) => (
+						<Link
+							key={planet.name}
+							to='/destination/$name'
+							params={{ name: planet.name }}
+						>
+							{planet.name}
+						</Link>
+					))}
+				</nav>
+				<Outlet />
+			</main>
+		</div>
+	);
 }
