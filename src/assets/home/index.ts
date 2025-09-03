aca63234e596539d34b12c152f5c1c3dcb42818c
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-09-03 15:55:08 +0100

    Barrel import home page backgrounds

---
+++ src/assets/home/index.ts
@@ -0,0 +1,5 @@
+import bgMobile from './background-home-mobile.jpg';
+import bgTablet from './background-home-tablet.jpg';
+import bgDesktop from './background-home-desktop.jpg';
+
+export { bgMobile, bgTablet, bgDesktop };
