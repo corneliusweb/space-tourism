47caa1164839bfb272d9e0ba7ec4da1de8f47ff9
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-09-04 12:05:54 +0100

    Implement responsive design of tech page

--- src/routes/technology/$technology.tsx
+++ src/routes/technology/$technology.tsx
@@ -15,10 +15,16 @@ export const Route = createFileRoute('/technology/$technology')({
 function TechnologyPage() {
 	const tech = Route.useLoaderData();
 	return (
-		<div>
-			<p>The terminology...</p>
-			<h2>{tech.name}</h2>
-			<p>{tech.description}</p>
-		</div>
+		<>
+			<p className='text-lg uppercase opacity-50 font-serif tablet:text-2xl lg:text-md-600'>
+				The terminology...
+			</p>
+			<h2 className='font-serif uppercase text-2xl py-6 tablet:text-sm-700 lg:text-md-700'>
+				{tech.name}
+			</h2>
+			<p className='text-blue-300 text-fs-400 leading-7 tablet:tracking-wide tablet:text-base lg:text-lg lg:tracking-normal lg:leading-9'>
+				{tech.description}
+			</p>
+		</>
 	);
 }
--- src/routes/technology/route.tsx
+++ src/routes/technology/route.tsx
@@ -6,10 +6,9 @@ import {
 	useLocation,
 } from '@tanstack/react-router';
 import { technologies } from '../../constants/data';
-import { bgDesktop } from '../../assets/technology';
-import { bgTablet } from '../../assets/technology';
-import { bgMobile } from '../../assets/technology';
+import { bgDesktop, bgMobile, bgTablet } from '../../assets/technology';
 import { useBodyBackground } from '../../constants/useBodyBackground';
+import PageHeading from '../../components/PageHeading';
 
 export const Route = createFileRoute('/technology')({
 	component: TechnologyLayout,
@@ -43,31 +42,52 @@ function TechnologyLayout() {
 		: undefined;
 	if (!tech) return <p>Technology not found</p>;
 
+	const handleTechImage = () => {
+		let techImage;
+		const width = window.innerWidth;
+
+		if (width >= 640 && width < 1024) {
+			techImage = tech.images.landscape;
+		} else {
+			techImage = tech.images.portrait;
+		}
+
+		return techImage;
+	};
+
 	return (
 		<>
-			<section>
-				<div>
-					<img src={tech.images.portrait} alt={tech.name} />
-				</div>
-			</section>
-			<section>
-				<nav>
-					<ul className='flex '>
-						{technologies.map((tech, index) => (
-							<li key={tech.id}>
-								<Link
-									to='/technology/$technology'
-									params={{ technology: tech.id }}
-								>
-									{index + 1}
-									<span className='sr-only'>{tech.name}</span>
-								</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-				<Outlet />
-			</section>
+			<PageHeading number='03' heading='Space Launch 101' />
+			<div className='my-16 lg:flex lg:flex-row-reverse lg:items-center lg:w-tech-content-lg lg:float-right lg:gap-10'>
+				<img src={handleTechImage()} alt={tech.name} className='tablet:h-tech-img-md lg:h-tech-img-lg lg:w-tech-img-lg' />
+
+				<section className='text-center px-4 tablet:px-0 lg:flex lg:text-left lg:gap-15 lg:w-tech-textbox-lg'>
+					<nav>
+						<ul className='flex space-x-4 justify-center py-8 pb-10 lg:flex-col lg:items-center lg:space-x-0 lg:py-0 lg:pb-0 lg:justify-between lg:h-full'>
+							{technologies.map((tech, index) => (
+								<li key={tech.id}>
+									<button>
+										<Link
+											to='/technology/$technology'
+											params={{ technology: tech.id }}
+											className='rounded-full flex items-center justify-center w-10 h-10 transition-all duration-300 ease-linear text-lg border-2 border-white/25 hover:bg-white hover:text-dark-blue tablet:w-14 tablet:h-14 tablet:text-2xl lg:w-20 lg:h-20 lg:text-md-600'
+											activeProps={{
+												className: 'bg-white text-dark-blue',
+											}}
+										>
+											{index + 1}
+											<span className='sr-only'>{tech.name}</span>
+										</Link>
+									</button>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<div className='tablet:max-w-lg mx-auto'>
+						<Outlet />
+					</div>
+				</section>
+			</div>
 		</>
 	);
 }
