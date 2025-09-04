9815e50d20f8389614c3b9500a4442f8dcca6165
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-09-04 08:17:56 +0100

    Implement responsiveness for crew page

--- src/routes/crew/$crew.tsx
+++ src/routes/crew/$crew.tsx
@@ -3,8 +3,8 @@ import { crews } from '../../constants/data';
 
 export const Route = createFileRoute('/crew/$crew')({
 	component: CrewMember,
-	loader: ({ params}) => {
-		const crew = crews.find((c) => c.id === params.crew );
+	loader: ({ params }) => {
+		const crew = crews.find((c) => c.id === params.crew);
 		if (!crew) {
 			throw notFound();
 		}
@@ -16,13 +16,15 @@ function CrewMember() {
 	const crew = Route.useLoaderData();
 	return (
 		<>
-			<p className='uppercase text-white/50 font-serif text-lg my-7'>
+			<p className='uppercase text-white/50 font-serif text-lg mb-2 tablet:text-fs-700 lg:text-md-600'>
 				{crew.role}
 			</p>
-			<h2 className='uppercase tracking-wide font-serif text-sm-700'>
+			<h2 className='uppercase tracking-wide font-serif text-fs-700 tablet:text-sm-700 lg:text-fs-800 lg:tracking-normal'>
 				{crew.name}
 			</h2>
-			<p className='text-base font-sans my-4'>{crew.bio}</p>
+			<p className='text-fs-400 leading-7 font-sans mt-4 tablet:text-base lg:text-lg lg:leading-9 text-blue-300'>
+				{crew.bio}
+			</p>
 		</>
 	);
 }
--- src/routes/crew/route.tsx
+++ src/routes/crew/route.tsx
@@ -39,36 +39,39 @@ function Crew() {
 	if (!crew) return <p>Crew member not found!</p>;
 
 	return (
-		<>
+		<div className='lg:w-crew-content-lg mx-auto'>
 			<div className='flex'>
 				<PageHeading number='02' heading='Meet your crew' />
 			</div>
-			<section className='w-lg mx-auto text-center'>
-				<Outlet />
-				<nav>
-					<ul className='flex gap-5 justify-center'>
-						{crews.map((crew) => (
-							<li key={crew.id}>
-								<Link
-									to='/crew/$crew'
-									params={{ crew: crew.id }}
-									className={`font-bold text-6xl text-white/17 inline-block`}
-									activeProps={{
-										className: 'text-white/100',
-									}}
-								>
-									. <span className='sr-only'>{crew.name}</span>
-								</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-			</section>
-			<section>
-				<div>
-					<img src={crew.image} alt={crew.name} />
-				</div>
-			</section>
-		</>
+			<div className='lg:flex lg:items-center lg:justify-between lg:min-h-crew-textbox-lg lg:relative lg:gap-5'>
+				<section className='text-center tablet:w-lg tablet:mx-auto lg:text-left lg:mx-0 lg:w-crew-textbox-lg'>
+					<Outlet />
+					<nav className='mb-11 mt-4 lg:absolute lg:left-0 lg:bottom-0'>
+						<ul className='flex gap-5 justify-center items-center lg:justify-start'>
+							{crews.map((crew) => (
+								<li key={crew.id}>
+									<Link
+										to='/crew/$crew'
+										params={{ crew: crew.id }}
+										className={`font-bold text-6xl text-white/17 inline-block`}
+										activeProps={{
+											className: 'text-white/100',
+										}}
+									>
+										. <span className='sr-only'>{crew.name}</span>
+									</Link>
+								</li>
+							))}
+						</ul>
+					</nav>
+				</section>
+
+				<img
+					src={crew.image}
+					alt={crew.name}
+					className='w-crew-img mx-auto tablet:w-crew-img-md block'
+				/>
+			</div>
+		</div>
 	);
 }
